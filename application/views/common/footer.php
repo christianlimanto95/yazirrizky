24d627ecfbcc6c1426f7756a068bfecf9332bf31
christianlimanto95 <christianlimanto95@gmail.com>
2017-08-18 09:49:18 +0700

    css

--- application/views/common/footer.php
+++ application/views/common/footer.php
@@ -1,5 +1,7 @@
 </div>
-<h1>THIS IS FOOTER</h1>
+<div class="footer">
+    This is footer
+</div>
 <script src="<?php echo base_url("assets/js/common/default.js"); ?>" defer></script>
 <script src="<?php echo base_url("assets/js/" . $page_name . ".js"); ?>" defer></script>
 </body>
--- application/views/common/header.php
+++ application/views/common/header.php
@@ -3,6 +3,7 @@
 	<title><?php echo $title; ?></title>
 	
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/common/default.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/common/header.css"); ?>" />
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/" . $page_name . ".css"); ?>" />
 	
 	<script src="<?php echo base_url("assets/js/common/jquery-3.2.1.min.js"); ?>" defer></script>
@@ -10,5 +11,6 @@
 	<?php echo $additional_files; ?>
 </head>
 <body>
+<div class="header">
+</div>
 <div class="container">
-	<h1>THIS IS HEEADER</h1>
